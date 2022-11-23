b7040c124fc1f02c0aeb061ada910603d1b2bb5a
Greta56 <greta.gabrenaite@gmail.com>
2022-11-23 19:35:50 +0200

    no message

--- image.py
+++ image.py
@@ -1,6 +1,7 @@
 import glob
 import string
 import time
+from datetime import datetime
 from multiprocessing.pool import ThreadPool
 from typing import Any
 
@@ -35,31 +36,31 @@ def filterImages(images: list, thresholdWidth: int, thresholdHeight: int, mypath
         image = cv2.imread(str(mypath) + '/' + i)
         height = image.shape[0]
         width = image.shape[1]
-
+        now = datetime.now()
+        date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
         if (width <= thresholdWidth and height <= thresholdHeight):
             img_rst = cv2.GaussianBlur(image, (9,9), cv2.BORDER_DEFAULT)
-            cv2.imwrite(str(resultPath) + str(count)+'.png', img_rst)
+            cv2.imwrite(str(resultPath) + str(date_time) + str(count)+'.png', img_rst)
             count += 1
 
         elif (width <= thresholdWidth and height >= thresholdHeight):
             img_rst = cv2.GaussianBlur(image, (5, 5), cv2.BORDER_DEFAULT)
-            cv2.imwrite(str(resultPath) + str(count) + '.png', img_rst)
+            cv2.imwrite(str(resultPath) + + str(date_time) + str(count) + '.png', img_rst)
             count += 1
 
 if __name__ == '__main__':
     highResolutionImages = list(readImages(highResolutionPath))
     lowResulutionImages = list(readImages(lowResolutionPath))
 
-    num_process = 10
+    num_process = 2
     task = filterImages(highResolutionImages, 1000, 1000, highResolutionPath)
     task2 = filterImages(lowResulutionImages, 1000, 1000, lowResolutionPath)
     print(f'MESSAGE: Running {num_process} process')
     ThreadPool(num_process).imap_unordered(task, highResolutionImages)
-
-    end = time.time()
-    print('Time taken to resize {}'.format(len(highResolutionImages)))
-    print(end - start)
+    end1 = time.time()
     ThreadPool(num_process).imap_unordered(task2, lowResulutionImages)
-    end = time.time()
-    print('Time taken to resize {}'.format(len(lowResulutionImages)))
-    print(end - start)
+    end2 = time.time()
+    print('Time taken to blur {}'.format(len(highResolutionImages)))
+    print(end1 - start)
+    print('Time taken to blur {}'.format(len(lowResulutionImages)))
+    print(end2 - start)
