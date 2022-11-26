b4ccedc1a9e23015e157abe40912823bec7ecd2e
Greta56 <greta.gabrenaite@gmail.com>
2022-11-26 19:08:24 +0200

    sutvarkytas failas, paruoštas gynimui

--- image.py
+++ image.py
@@ -11,12 +11,12 @@ import os, os.path
 import cv2
 import time
 
-start = time.time()
-
-resultPathHigh = 'C:/Users/Vartotojas/OneDrive - Kaunas University of Technology/Darbalaukis/IN_Lygiagretus/ResultsHigh/'
-resultPathLow = 'C:/Users/Vartotojas/OneDrive - Kaunas University of Technology/Darbalaukis/IN_Lygiagretus/ResultsLow/'
-highResolutionPath = "C:/Users/Vartotojas/OneDrive - Kaunas University of Technology/Darbalaukis/IN_Lygiagretus/HighResolutionImages"
-lowResolutionPath = "C:/Users/Vartotojas/OneDrive - Kaunas University of Technology/Darbalaukis/IN_Lygiagretus/LowResolutionImages"
+resultPathSmall = 'C:/Users/Vartotojas/OneDrive - Kaunas University of Technology/Darbalaukis/IN_Lygiagretus/ResultsSmall/'
+resultPathMedium = 'C:/Users/Vartotojas/OneDrive - Kaunas University of Technology/Darbalaukis/IN_Lygiagretus/ResultsMedium/'
+resultPathLarge = 'C:/Users/Vartotojas/OneDrive - Kaunas University of Technology/Darbalaukis/IN_Lygiagretus/ResultsLarge/'
+smallResolutionPath = "C:/Users/Vartotojas/OneDrive - Kaunas University of Technology/Darbalaukis/IN_Lygiagretus/SmallResolutionImages10"
+mediumResolutionPath = "C:/Users/Vartotojas/OneDrive - Kaunas University of Technology/Darbalaukis/IN_Lygiagretus/MediumResolutionImages10"
+largeResolutionPath = "C:/Users/Vartotojas/OneDrive - Kaunas University of Technology/Darbalaukis/IN_Lygiagretus/LargeResolutionImages10"
 
 def readImages(path):
 
@@ -32,7 +32,7 @@ def readImages(path):
     return images
 
 
-def filterImages(images: list, resultPath: Any, mypath: Any):
+def blurImages(images: list, resultPath: Any, mypath: Any):
     count = 0
     for i in images:
         image = cv2.imread(str(mypath) + '/' + i)
@@ -41,18 +41,27 @@ def filterImages(images: list, resultPath: Any, mypath: Any):
         count += 1
 
 if __name__ == '__main__':
-    highResolutionImages = list(readImages(highResolutionPath))
-    lowResulutionImages = list(readImages(lowResolutionPath))
+    smallResolutionImages = list(readImages(smallResolutionPath))
+    mediumResolutionImages = list(readImages(mediumResolutionPath))
+    largeResolutionImages = list(readImages(largeResolutionPath))
 
-    num_process = 10
-    task = filterImages(highResolutionImages, resultPathHigh, highResolutionPath)
-    task2 = filterImages(lowResulutionImages, resultPathLow, lowResolutionPath)
+    num_process = 200
     print(f'MESSAGE: Running {num_process} process')
-    ThreadPool(num_process).imap_unordered(task, highResolutionImages)
+    start1 = time.time()
+    ThreadPool(num_process).imap_unordered(blurImages(smallResolutionImages, resultPathSmall, smallResolutionPath), smallResolutionImages)
+    print(f'MESSAGE: Small images are finished bluring')
     end1 = time.time()
-    ThreadPool(num_process).imap_unordered(task2, lowResulutionImages)
+    start2 = time.time()
+    ThreadPool(num_process).imap_unordered(blurImages(mediumResolutionImages, resultPathMedium, mediumResolutionPath), mediumResolutionImages)
+    print(f'MESSAGE: Medium images are finished bluring')
     end2 = time.time()
-    print('Time taken to blur {}'.format(len(highResolutionImages)))
-    print(end1 - start)
-    print('Time taken to blur {}'.format(len(lowResulutionImages)))
-    print(end2 - start)
+    start3 = time.time()
+    ThreadPool(num_process).imap_unordered(blurImages(largeResolutionImages, resultPathLarge, largeResolutionPath), largeResolutionImages)
+    print(f'MESSAGE: Large images are finished bluring')
+    end3 = time.time()
+    print('Time taken to blur small images {}'.format(len(smallResolutionImages)))
+    print(end1 - start1)
+    print('Time taken to blur medium images {}'.format(len(mediumResolutionImages)))
+    print(end2 - start2)
+    print('Time taken to blur large images {}'.format(len(largeResolutionImages)))
+    print(end3 - start3)
