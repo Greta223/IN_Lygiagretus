53538fb9450496d56e7c7a95b624f676d7d0816b
Greta56 <greta.gabrenaite@gmail.com>
2022-11-26 12:38:31 +0200

    pakeista failo struktura

--- image.py
+++ image.py
@@ -13,9 +13,11 @@ import time
 
 start = time.time()
 
-resultPath = 'C:/Users/Vartotojas/OneDrive - Kaunas University of Technology/Darbalaukis/IN_Lygiagretus/Results/'
+resultPathHigh = 'C:/Users/Vartotojas/OneDrive - Kaunas University of Technology/Darbalaukis/IN_Lygiagretus/ResultsHigh/'
+resultPathLow = 'C:/Users/Vartotojas/OneDrive - Kaunas University of Technology/Darbalaukis/IN_Lygiagretus/ResultsLow/'
 highResolutionPath = "C:/Users/Vartotojas/OneDrive - Kaunas University of Technology/Darbalaukis/IN_Lygiagretus/HighResolutionImages"
 lowResolutionPath = "C:/Users/Vartotojas/OneDrive - Kaunas University of Technology/Darbalaukis/IN_Lygiagretus/LowResolutionImages"
+
 def readImages(path):
 
     images = []
@@ -30,31 +32,21 @@ def readImages(path):
     return images
 
 
-def filterImages(images: list, thresholdWidth: int, thresholdHeight: int, mypath: Any):
+def filterImages(images: list, resultPath: Any, mypath: Any):
     count = 0
     for i in images:
         image = cv2.imread(str(mypath) + '/' + i)
-        height = image.shape[0]
-        width = image.shape[1]
-        now = datetime.now()
-        date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
-        if (width <= thresholdWidth and height <= thresholdHeight):
-            img_rst = cv2.GaussianBlur(image, (9,9), cv2.BORDER_DEFAULT)
-            cv2.imwrite(str(resultPath) + str(date_time) + str(count)+'.png', img_rst)
-            count += 1
-
-        elif (width <= thresholdWidth and height >= thresholdHeight):
-            img_rst = cv2.GaussianBlur(image, (5, 5), cv2.BORDER_DEFAULT)
-            cv2.imwrite(str(resultPath) + + str(date_time) + str(count) + '.png', img_rst)
-            count += 1
+        img_rst = cv2.GaussianBlur(image, (9,9), cv2.BORDER_DEFAULT)
+        cv2.imwrite(str(resultPath) + str(count)+'.png', img_rst)
+        count += 1
 
 if __name__ == '__main__':
     highResolutionImages = list(readImages(highResolutionPath))
     lowResulutionImages = list(readImages(lowResolutionPath))
 
-    num_process = 2
-    task = filterImages(highResolutionImages, 1000, 1000, highResolutionPath)
-    task2 = filterImages(lowResulutionImages, 1000, 1000, lowResolutionPath)
+    num_process = 10
+    task = filterImages(highResolutionImages, resultPathHigh, highResolutionPath)
+    task2 = filterImages(lowResulutionImages, resultPathLow, lowResolutionPath)
     print(f'MESSAGE: Running {num_process} process')
     ThreadPool(num_process).imap_unordered(task, highResolutionImages)
     end1 = time.time()
